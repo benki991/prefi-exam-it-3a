87788aef236c1aff7f77045f31270418f7c5f50b
Kevin Payos <kevinbarret.payos@gmail.com>
2024-05-25 21:49:55 +0800

    added subject model, factory, controller, routes, migration

---
+++ app/Models/Subject.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'subject_code',
+        'name',
+        'description',
+        'instructor',
+        'schedule',
+        'prelims',
+        'midterms',
+        'prefinals',
+        'finals',
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SubjectController;
 
 Route::get('/students', [StudentController::class, 'index']);
 Route::post('/students', [StudentController::class, 'create']);
 Route::get('/students/{id}', [StudentController::class, 'student']);
 Route::patch('/students/{id}', [StudentController::class, 'update']);
+
+Route::get('/students/{id}/subjects', [SubjectController::class, 'student']);
+Route::post('/students/{id}/subjects', [SubjectController::class, 'student']);
+Route::get('/students/{id}/subjects/{subject_id}', [SubjectController::class, 'student']);
+Route::patch('/students/{id}/subjects/{subject_id}', [SubjectController::class, 'student']);
